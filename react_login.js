76158d68b20b0f3a5293c1e2261ae86fc536cffa
ChaerinMin <43366923+ChaerinMin@users.noreply.github.com>
2020-03-20 17:51:42 -0400

    adding react

--- react_login.js
+++ react_login.js
@@ -1,12 +1,34 @@
+'use strict';
 import React, {Component} from 'react';
 import './Login_Page_styles.css';
+const e =React.createElement;
 
 class react_login extends Component{
     render(){
+
+        state = {
+            username:""
+        }
+
+        handleUsername = e=>{
+            this.setState({
+                username : e.target.value
+            });
+        }
         return (
-            <div>
-                
-            </div>
+            <div class="username">
+            <label id="username_label" for="username_box">Username:<a></a></label>
+            <input 
+                id="username_box" type="text" placeholder="DSC Username" required 
+                value = {this.state.username}
+                onChange = {this.handleUsername}
+            ></input><br></br>
+            <div><b>{this.state.username}</b></div>
+            </div> 
         );
     }
 }
+
+const domContainer = 
+document.querySelector('#login_react');
+ReactDOM.render(e(react_login),domeContainer);
