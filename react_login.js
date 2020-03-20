2b1a0400e2f092b842be3724dd6378430dfe929f
ChaerinMin <43366923+ChaerinMin@users.noreply.github.com>
2020-03-20 18:15:18 -0400

    6:15

--- react_login.js
+++ react_login.js
@@ -4,31 +4,37 @@ import './Login_Page_styles.css';
 const e =React.createElement;
 
 class react_login extends Component{
+    state = {
+        username:""
+    }
+
+    handleUsername = e=>{
+        this.setState({
+            username : e.target.value
+        });
+    }
+
     render(){
 
-        state = {
-            username:""
-        }
+        
+        // return (
+        //     <div class="username">
+        //     <label id="username_label" for="username_box">Username:<a></a></label>
+        //     <input 
+        //         id="username_box" type="text" placeholder="DSC Username" required 
+        //         value = {this.state.username}
+        //         onChange = {this.handleUsername}
+        //     ></input><br></br>
+        //     <div><b>{this.state.username}</b></div>
+        //     </div> 
+        // );
+        return e(
+            {onChange : e=>this.setState({username:e.target.value})}
+        ) 
 
-        handleUsername = e=>{
-            this.setState({
-                username : e.target.value
-            });
-        }
-        return (
-            <div class="username">
-            <label id="username_label" for="username_box">Username:<a></a></label>
-            <input 
-                id="username_box" type="text" placeholder="DSC Username" required 
-                value = {this.state.username}
-                onChange = {this.handleUsername}
-            ></input><br></br>
-            <div><b>{this.state.username}</b></div>
-            </div> 
-        );
     }
 }
 
 const domContainer = 
-document.querySelector('#login_react');
+document.querySelector('#username_box');
 ReactDOM.render(e(react_login),domeContainer);
