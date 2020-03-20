1e35ac6a01d59d99d20b5eca639a6597312149fe
ChaerinMin <43366923+ChaerinMin@users.noreply.github.com>
2020-03-20 17:22:14 -0400

    starting basic react form

---
+++ react_login.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import './Login_Page_styles.css';
+
+class react_login extends Component{
+    render(){
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
